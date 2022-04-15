4f7c49350b039d675ddaee3920d741c07dd967b7
gecicidegisken <hayallerimyaziyor@gmail.com>
2022-04-15 03:03:53 +0300

    refactor: recreate entry voting

--- app.py
+++ app.py
@@ -4,31 +4,33 @@ from bson.objectid import ObjectId
 from flask_restful import reqparse, abort, Api, Resource
 
 app = Flask(__name__,instance_relative_config=True)
-# app.secret_key = 'kRzGGFpnat' 
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
 api = Api(app)
-print(app.secret_key)
+
 #dummy data
 entrys ={
-    'entry1':{'title':'I.R.S', 'content':'selam naber'},
-    'entry2':{'title':'Catcher In The Rye', 'content':'iyidir senden naber'}
+    'entry1':{'title':'I.R.S', 'content':'selam naber', 'upvotes':0 ,'downvotes':0},
+    'entry2':{'title':'Catcher In The Rye', 'content':'iyidir senden naber', 'upvotes':0 ,'downvotes':0}
 }
 users = {
     'user1':{'username':'yazar','password':'123'},
     'user2':{'username':'okuyucu','password':'123'}
 }
 
-#entry get, update actions
+#entry get, vote actions
 class Entry(Resource):
     def get(self, entry_id):
         #get an entry
         return {'title':entrys[entry_id]['title'],'content':entrys[entry_id]['content']}
     def put(self,entry_id):    
-        # update an entry
-        entrys[entry_id]['title']=request.form['title']
-        entrys[entry_id]['content']=request.form['content']
-        return {"title": entrys[entry_id]['title'],"content":entrys[entry_id]['content']}
+        # vote an entry
+        vote = int(request.form['vote'])
+        if vote==1:
+            entrys[entry_id]['upvotes']+=1
+        elif vote==0:
+            entrys[entry_id]['downvotes']+=1
+        return {"title": entrys[entry_id]['title'],"content":entrys[entry_id]['content'],"up":entrys[entry_id]['upvotes'], "down":entrys[entry_id]['downvotes']}
 
 #entry list all, post new and delete actions
 class EntryList(Resource):
@@ -41,19 +43,19 @@ class EntryList(Resource):
         content=request.form['content']
         entry_id = int(max(entrys.keys()).lstrip('entry'))+1
         entry_id = 'entry%i'%entry_id
-        entrys[entry_id]={'title':title,'content':content}
+        entrys[entry_id]={'title':title,'content':content, 'upvotes':0 ,'downvotes':0}
     def delete(self,entry_id):
         """delete entry"""
 
 
-#user create, delete and get actions
+#user register, delete and get actions
 class UserList(Resource):
     def get(self,user_id):
         #get a user
         return users[user_id]
 
     def post(self):
-        #create new user
+        #register new user
         username= request.form['u_username']
         password= request.form['u_password']
         user_id = int(max(users.keys()).lstrip('user'))+1
