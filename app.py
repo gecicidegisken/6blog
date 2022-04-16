54edaa2713f2ea91b8e36cec3b866192d4eaea64
gecicidegisken <hayallerimyaziyor@gmail.com>
2022-04-17 02:52:20 +0300

    chore: database integrated using mongoengine

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
-from sqlite3 import connect
 from flask import (Flask, request, session, url_for,redirect)
 from mongoengine import *
-
 from flask_restful import Api, Resource
 
 app = Flask(__name__,instance_relative_config=True)
@@ -10,90 +8,128 @@ app.config.from_pyfile('config.py')
 api = Api(app)
 connect('6blogdb')
 
-#dummy data
-entrys ={
-    'entry1':{'title':'I.R.S', 'content':'selam naber', 'upvotes':0 ,'downvotes':0},
-    'entry2':{'title':'Catcher In The Rye', 'content':'iyidir senden naber', 'upvotes':0 ,'downvotes':0}
-}
-users = {
-    'user1':{'username':'yazar','password':'123', 'usertype':"writer"},
-    'user2':{'username':'okuyucu','password':'123','usertype':"reader"}
-}
+#database schemas
 
-#entry get, vote actions
-class Entry(Resource):
+class User(Document):
+    username =StringField(required=True, unique=True)
+    password = StringField(required = True) #bunun tipi değişebilir
+    usertype = BooleanField(required =True) #1 = writer , 0 = reader
+
+class Entry(Document):
+    title=StringField(required=True)
+    content=StringField(required=True)
+    upvotes=IntField(required=True)
+    downvotes=IntField(required=True)
+    author = ReferenceField(User,reverse_delete_rule=CASCADE) #delete all of their entries if a user is deleted
+    
+
+# dummyuser1=User(username='yazar', password='123',usertype=1).save()
+# dummyuser2=User(username='okur', password='123',usertype=0).save()
+
+# dummypost1=Entry(title='Chinese Democracy', content='AAAAA', author=dummyuser1, downvotes=0, upvotes=0).save()
+# dummypost2=Entry(title='The Spaghetti Incident', content='BBBBB', author=dummyuser1, downvotes=0, upvotes=0).save()
+# dummypost3=Entry(title='The Spaghetti Incident', content='BBBBB', author=dummyuser1, downvotes=0, upvotes=0).save()
+
+#entry: get, vote actions
+class SingleEntry(Resource):
     def get(self, entry_id):
         #get an entry
-        return {'title':entrys[entry_id]['title'],'content':entrys[entry_id]['content']}
+        for e in Entry.objects(id=entry_id):
+            return {'title':e.title,'content':e.content}
+            
     def put(self,entry_id):    
-        # vote an entry
-        print(session)
+        #vote an entry
         if 'username' in session:
             vote = int(request.form['vote'])
-            if vote==1:
-                entrys[entry_id]['upvotes']+=1
-            elif vote==0:
-                entrys[entry_id]['downvotes']+=1
+            for e in Entry.objects(id=entry_id):
+                if vote==1:
+                    e.upvotes =e.upvotes+1
+                    e.save()
+                elif vote==0:
+                    e.downvotes =e.downvotes+1
+                    e.save()
+                return ({'downvotes':int(e.downvotes),'upvotes':int(e.upvotes)})
         else:
             return("hata:oy kullanmak icin giris yapmalisiniz")
-        return {"title": entrys[entry_id]['title'],"content":entrys[entry_id]['content'],"up":entrys[entry_id]['upvotes'], "down":entrys[entry_id]['downvotes']}
 
-#entry list all, post new and delete actions
+#entry: list all, post new and delete actions
 class EntryList(Resource):
     def get(self):
-        #get all entries
-        return entrys
+        #get all entries (return düzenlenecek)
+        for e in Entry.objects:
+            print(e.title)
+        return ('No entry found')
     def post(self):
-        #new entry
+        #new entry  
         title=request.form['title']
         content=request.form['content']
-        entry_id = int(max(entrys.keys()).lstrip('entry'))+1
-        entry_id = 'entry%i'%entry_id
-        entrys[entry_id]={'title':title,'content':content, 'upvotes':0 ,'downvotes':0}
+        if 'username' in session:
+            for u in User.objects(username=session['username']):
+                author=u
+            newEntry=Entry(title=title, content=content, author=author, downvotes=0, upvotes=0).save()
+            return({"title":newEntry.title,"author":newEntry.author})
+        else:
+            return({'hata':'yazi yazmak icin giris yapmalisiniz'})
     def delete(self,entry_id):
-        """delete entry"""
+        # delete entry by id
+        for e in Entry.objects(id=entry_id):
+            e.delete()
+            return {"onay":"yazi silindi"}
+        return({"hata":"yazi silinemedi"})
 
-
-#user register, delete and get actions
+#user: register, delete and get actions
 class UserList(Resource):
     def get(self,user_id):
-        #get a user
-        return users[user_id]
+        #get a user's informationee
+        for u in User.objects(id=user_id):
+            return {'username':u.username, 'password':u.password,'usertype':u.usertype}
+        return({"hata":"kullanici bulunamadi"})
 
     def post(self):
         #register new user
         username= request.form['username']
         password= request.form['password']
-        user_id = int(max(users.keys()).lstrip('user'))+1
-        user_id = 'user%i'%user_id
-        users[user_id] ={'username':username,'password':password}
-    def delete(self):
-        """delete user"""
+        usertype= request.form['usertype']
+        for u in User.objects(username=username):
+            if u:
+                return({"hata":"kullanici adi kullaniliyor"})
+        newUser=User(username=username,password=password,usertype=usertype).save()
+        return {'username':newUser.username, 'password':newUser.password,'usertype':newUser.usertype}
+    def delete(self,user_id):
+         #delete user
+        for u in User.objects(id=user_id):
+            u.delete()
+            return {"onay":"kullanici silindi"}
+        return({"hata":"kullanici silinemedi"})
 
-#user login logout actions
-class User(Resource):
+#user: login and logout actions
+class SingleUser(Resource):
     def get(self):
         #get the page with login form
         return()
     def post(self):
         #user login --credental check
         username = request.form['username']
-        session['username']=username  
-        print(session)
-        return("giris:basarili")
+        password = request.form['password']
+        for u in User.objects:
+            if u.username== username and u.password == password:
+                session['username']=username  
+                print(session)
+                return({"onay":"giris basarili"})
+        return ({"hata":"giris basarisiz"})
     def delete(self):
         #user logout
-        print(session)
         session.pop('username',None)
-        return("cikis:basarili")
+        print(session)
+        return({"onay":"cikis basarili"})
 
 
          
 api.add_resource(EntryList,'/', '/entries', '/entries/new')     
-api.add_resource(Entry,'/entries/<string:entry_id>')
+api.add_resource(SingleEntry,'/entries/<string:entry_id>')
 
-api.add_resource(UserList,'/users/<string:user_id>')
-api.add_resource(User,'/login')
+api.add_resource(UserList,'/users/<string:user_id>','/users/register','/users')
+api.add_resource(SingleUser,'/login')
 
 if __name__ == "__main__": 
     app.run(debug=True)
